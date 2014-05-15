2f885a94ce5c5b31458874e2b9733fad2bbe7fdd
MBielmeier <mexle.b@web.de>
2014-05-15 13:49:12 +0200

    Refactoring with Method: Replace Temp with Query
    replacing totalAmount with a charge method

--- src/Customer.java
+++ src/Customer.java
@@ -18,7 +18,6 @@ class Customer {
 	};
 
 	public String statement() {
-		double totalAmount = 0;
 		int frequentRenterPoints = 0;
 		Enumeration enum_rentals = rentals.elements();
 		String result = "Rental Record for " + this.getName() + "\n";
@@ -35,10 +34,9 @@ class Customer {
 			result += "\t" + each.getMovie().getTitle() + "\t" + "\t"
 					+ each.getDaysRented() + "\t" + String.valueOf(each.getCharge())
 					+ "\n";
-			totalAmount += each.getCharge();
 		}
 		// add footer lines
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+		result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
 		result += "You earned " + String.valueOf(frequentRenterPoints)
 				+ " frequent renter points";
 		return result;
@@ -46,5 +44,20 @@ class Customer {
 
 	private double amountFor(Rental aRental){
 		return aRental.getCharge();
+	}
+	private double getTotalCharge(){
+		
+		double result=0;
+		
+		Enumeration enum_rentals = rentals.elements();
+		while(enum_rentals.hasMoreElements()){
+			
+			Rental each = (Rental) enum_rentals.nextElement();
+			result+=each.getCharge();
+		}
+		
+		
+		
+		return result;
 	}
 }
